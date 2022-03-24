56301e072c4c62d7162379e14f93a067fe5221f2
Anuj kumar <anuj.kumarr.97@gmail.com>
2022-03-24 10:46:03 +0530

    [Anuj]Add: Create Table with firstName,address,city,state,zip

---
+++ UC2AddressBookSystemSQLQuery.sql
@@ -0,0 +1,14 @@
+--UC-1
+create database AddressBookServices;
+
+--UC-2
+create table Address_Book_Table
+(FirstName varchar(100),
+LastName varchar(100),
+Address varchar(250),
+City varchar(100),
+State varchar(100),
+zip BigInt,
+PhoneNumber BigInt,
+Email varchar(200)
+)
